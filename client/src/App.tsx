4bc12a0cfa811acd9dc52285a7d88b3dfc5f6268
mini <0miiii@naver.com>
2023-04-06 22:34:55 +0900

    Implement routerProvider for page movement

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import { RouterProvider } from 'react-router-dom';
+import { routers } from './router';
 
 function App() {
   return (
     <>
       <Header />
+      <RouterProvider router={routers} />
     </>
   );
 }
