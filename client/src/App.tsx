4b79119636fcdd498e4fdaec9375389d005607d9
mini <0miiii@naver.com>
2023-04-06 22:33:42 +0900

    Add header component to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <div>Qumunity</div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
