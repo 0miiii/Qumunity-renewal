126a247f473e70328aa7b974fe06d0b1f30da5bd
0miiii <0miiii@naver.com>
2023-04-07 16:13:51 +0900

    Add SignUpSubmitHandler

--- client/src/components/SignUpForm/SignUpForm.tsx
+++ client/src/components/SignUpForm/SignUpForm.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { TextField, Button } from '@mui/material';
-import * as Styled from './SignUpForm.style';
+import React from "react";
+import { TextField, Button } from "@mui/material";
+import * as Styled from "./SignUpForm.style";
 
 const SignUpForm = () => {
+  const SignUpSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
-    <Styled.Container>
-      <TextField id="nickname" label="닉네임" variant="outlined" />
-      <TextField id="email" label="이메일" variant="outlined" />
-      <TextField id="password" label="비밀번호" variant="outlined" />
-      <TextField id="password-check" label="비밀번호 확인" variant="outlined" />
-      <Button variant="contained">가입하기</Button>
+    <Styled.Container onSubmit={SignUpSubmitHandler}>
+      <TextField label="닉네임" variant="outlined" />
+      <TextField label="이메일" variant="outlined" />
+      <TextField label="비밀번호" variant="outlined" />
+      <TextField label="비밀번호 확인" variant="outlined" />
+      <Button variant="contained" type="submit">
+        가입하기
+      </Button>
     </Styled.Container>
   );
 };
