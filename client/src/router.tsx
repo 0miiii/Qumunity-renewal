86f0651ab4686bb0cad4f42ccece8d6bdde94bc5
0miiii <0miiii@naver.com>
2023-04-07 22:13:46 +0900

    Add variables to make route maintenance easier

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
-import AuthPage from './pages/AuthPage/AuthPage';
-import MyPage from './pages/MyPage/MyPage';
-import DetailPage from './pages/DetailPage/DetailPage';
-import TagListPage from './pages/TagListPage/TagListPage';
-import UserListPage from './pages/UserListPage/UserListPage';
-import AuthGaurdLayout from './layouts/AuthGaurdLayout';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import MyPage from "./pages/MyPage/MyPage";
+import DetailPage from "./pages/DetailPage/DetailPage";
+import TagListPage from "./pages/TagListPage/TagListPage";
+import UserListPage from "./pages/UserListPage/UserListPage";
+import AuthGaurdLayout from "./layouts/AuthGaurdLayout";
 
 interface IRouter {
   id: number;
@@ -17,59 +17,69 @@ interface IRouter {
   withNavi: boolean;
 }
 
+export const PATH = {
+  MAIN: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+  MYPAGE: "/mypage/:id",
+  DETAIL: "/detail/:id",
+  TAG_LIST: "/taglist",
+  USER_LIST: "/userlist",
+};
+
 const routerData: IRouter[] = [
   {
     id: 0,
-    path: '/',
-    label: 'Main',
+    path: PATH.MAIN,
+    label: "Main",
     element: <MainPage />,
     withAuth: false,
     withNavi: true,
   },
   {
     id: 1,
-    path: '/sign-in',
-    label: 'SignIn',
+    path: PATH.SIGN_IN,
+    label: "SignIn",
     element: <AuthPage />,
     withAuth: false,
     withNavi: false,
   },
   {
     id: 2,
-    path: '/sign-up',
-    label: 'SignUp',
+    path: PATH.SIGN_UP,
+    label: "SignUp",
     element: <AuthPage />,
     withAuth: false,
     withNavi: false,
   },
   {
     id: 3,
-    path: '/mypage/:id',
-    label: 'Main',
+    path: PATH.MYPAGE,
+    label: "MyPage",
     element: <MyPage />,
     withAuth: true,
     withNavi: false,
   },
   {
     id: 4,
-    path: '/detail/:id',
-    label: 'Detail',
+    path: PATH.DETAIL,
+    label: "Detail",
     element: <DetailPage />,
     withAuth: true,
     withNavi: false,
   },
   {
     id: 5,
-    path: '/taglist',
-    label: '태그',
+    path: PATH.TAG_LIST,
+    label: "태그",
     element: <TagListPage />,
     withAuth: false,
     withNavi: true,
   },
   {
     id: 6,
-    path: '/userlist',
-    label: '유저',
+    path: PATH.USER_LIST,
+    label: "유저",
     element: <UserListPage />,
     withAuth: false,
     withNavi: true,
