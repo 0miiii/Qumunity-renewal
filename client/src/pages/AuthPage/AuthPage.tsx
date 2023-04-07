0d9256babadf5c26845bc7ec02e18bf35e6f2405
0miiii <0miiii@naver.com>
2023-04-07 22:15:40 +0900

    Use PATH variable instead of string

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import * as Styled from './AuthPage.style';
-import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import SignInForm from '../../components/SignInForm/SignInForm';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import * as Styled from "./AuthPage.style";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import SignInForm from "../../components/SignInForm/SignInForm";
+import { PATH } from "../../router";
 
 const AuthPage = () => {
   const location = useLocation().pathname;
-  const AuthForm = location === '/sign-up' ? <SignUpForm /> : <SignInForm />;
+  const AuthForm = location === PATH.SIGN_UP ? <SignUpForm /> : <SignInForm />;
 
-  console.log('location', location);
   return (
     <Styled.Container>
       <div>로고</div>
