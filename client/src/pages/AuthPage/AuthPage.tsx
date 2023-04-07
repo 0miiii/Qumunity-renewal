0482df3735eb1d06adaaa95998aece7fb0374d0e
0miiii <0miiii@naver.com>
2023-04-07 14:40:05 +0900

    Fix wrong path

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import * as Styled from './SignUpPage.style';
+import * as Styled from './AuthPage.style';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import SignInForm from '../../components/SignInForm/SignInForm';
 
