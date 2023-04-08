6543495d6e1bcc95046830bcdfb5be5a0c648f5f
mini <0miiii@naver.com>
2023-04-08 15:14:35 +0900

    Implement logic to save user to db

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,5 +1,11 @@
-import User from "../models/userModel";
+import UserModel, { IUser } from "../models/userModel";
 
-class UserService {}
-
-export default UserService;
+export const createUser = async (user: IUser) => {
+  try {
+    const newUser = new UserModel(user);
+    await newUser.save();
+    return newUser;
+  } catch (err) {
+    throw new Error(`Error while saving user to db ${err}`);
+  }
+};
