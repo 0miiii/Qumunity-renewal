6183269adee2ad5c34f4adc8b8e97577bf373794
mini <0miiii@naver.com>
2023-04-08 15:03:07 +0900

    Add return type

--- server/src/libs/hash.ts
+++ server/src/libs/hash.ts
@@ -1,7 +1,7 @@
 import { hash, compare } from "bcryptjs";
 
 export const hashPassword = async (password: string) => {
-  const hashedPassword = await hash(password, 12);
+  const hashedPassword: string = await hash(password, 12);
   return hashedPassword;
 };
 
@@ -9,6 +9,6 @@ export const comparePassword = async (
   enteredPassword: string,
   hashedPassword: string
 ) => {
-  const isValid = await compare(enteredPassword, hashedPassword);
+  const isValid: boolean = await compare(enteredPassword, hashedPassword);
   return isValid;
 };
