6da380d96f654295b7feeca8a2823201d0ed752e
mini <0miiii@naver.com>
2023-04-08 15:37:10 +0900

    Update for clearer error handling

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,11 +1,11 @@
 import UserModel, { IUser } from "../models/userModel";
 
 export const createUser = async (user: IUser) => {
-  try {
-    const newUser = new UserModel(user);
-    await newUser.save();
-    return newUser;
-  } catch (err) {
-    throw new Error(`Error while saving user to db ${err}`);
+  const newUser = new UserModel(user).save();
+
+  if (!newUser) {
+    throw new Error("유저 생성에 실패했습니다.");
   }
+
+  return newUser;
 };
