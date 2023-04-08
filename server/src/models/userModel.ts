71cdc27af04d934b5589b4e4942c64fd73d6b828
mini <0miiii@naver.com>
2023-04-08 15:05:09 +0900

    Add middleware and method to schema for password security

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,22 +1,33 @@
 import { Schema, model, Document } from "mongoose";
+import { hashPassword, comparePassword } from "../libs/hash";
 
 export interface IUser extends Document {
   email: string;
   password: string;
   nickName: string;
   photoURL: string;
+  verifyPassword: (enteredPassword: string) => Promise<boolean>;
 }
 
 const userSchema = new Schema<IUser>(
   {
-    email: String,
-    password: String,
-    nickName: String,
-    photoURL: String,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    nickName: { type: String, required: true },
+    photoURL: { type: String, required: true },
   },
   { collection: "users" }
 );
 
-const User = model<IUser>("User", userSchema);
+userSchema.pre<IUser>("save", async function (next) {
+  this.password = await hashPassword(this.password);
+  next();
+});
 
-export default User;
+userSchema.methods.verifyPassword = async function (enteredPassword: string) {
+  return await comparePassword(enteredPassword, this.password);
+};
+
+const UserModel = model<IUser>("User", userSchema);
+
+export default UserModel;
