0616037392358dd79588776aa6095c997ca19046
0miiii <0miiii@naver.com>
2023-04-09 00:13:12 +0900

    Add container design

---
+++ client/src/components/QuestionInput/QuestionInput.style.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 840px;
+  padding: 20px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  margin: 0 auto;
+  background-color: antiquewhite;
+`;
