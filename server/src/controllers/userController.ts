cb4963d180f87512e8b80ed242efbf284ba4a327
mini <0miiii@naver.com>
2023-04-08 15:45:58 +0900

    Implement signup controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import * as userService from "../services/userService";
 
-export const createUser = (req: Request, res: Response) => {
-  res.send("회원가입");
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    await userService.createUser({ ...req.body, photoURL: "test" });
+    res.status(200).send({
+      message: "Qumunity에 오신 것을 환영합니다",
+      token: "임시토큰",
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: `회원가입에 실패했습니다 ${err}`,
+    });
+  }
 };
 
 export const loginUser = (req: Request, res: Response) => {
