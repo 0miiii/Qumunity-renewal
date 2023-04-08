3bf38eaf3a7abe4b6fca88bc5a9885fe1fcd8abb
0miiii <0miiii@naver.com>
2023-04-09 00:14:45 +0900

    Implement input styles based on type

--- client/src/components/QuestionInput/QuestionInput.tsx
+++ client/src/components/QuestionInput/QuestionInput.tsx
@@ -1,12 +1,38 @@
 import React from "react";
+import * as Styled from "./QuestionInput.style";
+import QuestionTextInput from "./QuestionTextInput/QuestionTextInput";
+import QuestionTextarea from "./QuestionTextarea/QuestionTextarea";
+import QuestionTagInput from "./QuestionTagInput/QuestionTagInput";
+import QuestionEditor from "./QuestionEditor/QuestionEditor";
 
-const QuestionInput = () => {
+interface IQuestionInput {
+  title: string;
+  summary: string;
+  type: "title" | "summary" | "tag" | "editor";
+}
+
+const QuestionInput: React.FC<IQuestionInput> = ({ title, summary, type }) => {
+  let inputType: React.ReactNode;
+
+  if (type === "title") {
+    inputType = <QuestionTextInput />;
+  }
+  if (type === "summary") {
+    inputType = <QuestionTextarea />;
+  }
+  if (type === "tag") {
+    inputType = <QuestionTagInput />;
+  }
+  if (type === "editor") {
+    inputType = <QuestionEditor />;
+  }
+
   return (
-    <div>
-      <label htmlFor="title">제목</label>
-      <p>문단</p>
-      <input id="title" type="text" />
-    </div>
+    <Styled.Container>
+      <label htmlFor="title">{title}</label>
+      <p>{summary}</p>
+      {inputType}
+    </Styled.Container>
   );
 };
 
