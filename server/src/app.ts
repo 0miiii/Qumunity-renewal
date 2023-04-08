90bdf818c40758aac3c8dc1c0ffb8b3533bccba6
mini <0miiii@naver.com>
2023-04-08 13:51:45 +0900

    Use 'dbConnect' for app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from "express";
-import mongoose from "mongoose";
 import morgan from "morgan";
 import path from "path";
+import dotenv from "dotenv";
 import userRouter from "./routes/userRoute";
+import { dbConnect } from "./libs/mongoose";
+
+dotenv.config();
 
 const app: Application = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
@@ -12,13 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRouter);
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  // mongoose
-  //   .connect("")
-  //   .then(() => console.log(`mongoose connection successful`))
-  //   .catch((err) => console.log(err));
-
-  console.log(`Server listening on port ${port}`);
+app.listen(port, async () => {
+  await dbConnect();
+  console.log(`listening on port ${port}`);
 });
