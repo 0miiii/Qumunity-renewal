628b45e41fa25545c9ace1900520229265e5c375
mini <0miiii@naver.com>
2023-04-09 19:01:04 +0900

    Add tag input component

---
+++ client/src/components/TagInput/TagInput.style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  border: 1px solid #30363d;
+  border-radius: 3px;
+  background-color: var(--main-bgc);
+
+  input {
+    padding: 10px;
+  }
+`;
+
+export const TagGroup = styled.ul`
+  flex: 0 0 auto;
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  padding: 0 5px;
+
+  span {
+    margin-left: 3px;
+  }
+`;
---
+++ client/src/components/TagInput/TagInput.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import * as Styled from "./TagInput.style";
+
+const TagInput = () => {
+  const [tags, setTags] = useState<string[]>([]);
+
+  const tagAddHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" || event.key === " ") {
+      const enteredTag = event.currentTarget.value.trim();
+      if (!enteredTag) {
+        event.currentTarget.value = "";
+        return;
+      }
+      setTags((preTags) => [...preTags, `#${enteredTag}`]);
+      event.currentTarget.value = "";
+    }
+  };
+
+  return (
+    <Styled.Container>
+      <Styled.TagGroup>
+        {tags.map((el) => (
+          <li key={el}>{el}</li>
+        ))}
+      </Styled.TagGroup>
+      <input type="text" onKeyUp={tagAddHandler} />
+    </Styled.Container>
+  );
+};
+
+export default TagInput;
