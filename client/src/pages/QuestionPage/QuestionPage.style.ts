b02a358f5137c028182df3063c21300f1aada636
mini <0miiii@naver.com>
2023-04-09 19:09:19 +0900

    Add input to compose text

---
+++ client/src/pages/QuestionPage/QuestionPage.style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const InputContainer = styled.div`
+  width: 840px;
+  padding: 20px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  background-color: antiquewhite;
+`;
--- client/src/pages/QuestionPage/QuestionPage.tsx
+++ client/src/pages/QuestionPage/QuestionPage.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@mui/material";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import TagInput from "../../components/TagInput/TagInput";
+import * as Styled from "./QuestionPage.style";
 
 const QuestionPage = () => {
-  return <div>QuestionPage</div>;
+  return (
+    <Styled.Container>
+      <Styled.Form>
+        <Styled.InputContainer>
+          <label>제목을 입력해주세요</label>
+          <input type="text" />
+        </Styled.InputContainer>
+
+        <Styled.InputContainer>
+          <label>내용을 입력해주세요</label>
+          <Editor
+            previewStyle="tab"
+            height="400px"
+            useCommandShortcut={false}
+            initialEditType="wysiwyg"
+            toolbarItems={[
+              ["heading", "bold", "italic", "strike"],
+              ["hr", "quote"],
+              ["ul", "ol", "task"],
+              ["table", "link"],
+              ["code", "codeblock"],
+            ]}
+          />
+        </Styled.InputContainer>
+
+        <Styled.InputContainer>
+          <label>태그를 입력해주세요</label>
+          <TagInput />
+        </Styled.InputContainer>
+
+        <Button variant="contained">글 작성하기</Button>
+      </Styled.Form>
+    </Styled.Container>
+  );
 };
 
 export default QuestionPage;
