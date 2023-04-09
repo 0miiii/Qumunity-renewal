e319ebad700adeaa197ce2d2f9879210ef4e78ea
mini <0miiii@naver.com>
2023-04-09 21:08:00 +0900

    Add redux provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import GlobalStyle from './styles/Global.style';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import GlobalStyle from "./styles/Global.style";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
     <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </Provider>
 );
