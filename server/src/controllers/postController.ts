c0187032acbcadb7b7c18a9de159439282ef0def
0miiii <0miiii@naver.com>
2023-04-11 01:24:27 +0900

    Implement a controller to get all posts

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import PostModel from "../models/postModel";
 import * as UserService from "../services/userService";
 import * as PostService from "../services/postService";
 
@@ -34,5 +33,24 @@ export const createPost = async (
     return res
       .status(400)
       .json({ success: false, message: "글 작성에 실패하였습니다" });
+  }
+};
+
+export const getAllPost = async (_: Request, res: Response) => {
+  try {
+    const posts = await PostService.getAllPost();
+    return res.status(200).json({
+      success: true,
+      message: "모든 게시물을 응답합니다",
+      data: posts,
+    });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(400)
+      .json({
+        success: false,
+        message: "모든 게시물을 불러오는데 실패하였습니다",
+      });
   }
 };
