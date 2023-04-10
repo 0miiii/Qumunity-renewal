ef335f5bc45b6bfa8500326cc178226e7c3c3658
mini <0miiii@naver.com>
2023-04-10 17:58:15 +0900

    Use post controller

--- server/src/routes/postRoute.ts
+++ server/src/routes/postRoute.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import * as postController from "../controllers/postController";
 
 const router = Router();
 
-router.post("/register", () => {});
+router.post("/register", postController.createPost);
 
 export default router;
