cb8e9930d3f0e8b4f3d54a872d05fab28bc74040
mini <0miiii@naver.com>
2023-04-10 18:15:53 +0900

    Add middleware that performs auth for post registration

--- server/src/routes/postRoute.ts
+++ server/src/routes/postRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import * as postController from "../controllers/postController";
+import { validateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/register", postController.createPost);
+router.post("/register", validateToken, postController.createPost);
 
 export default router;
