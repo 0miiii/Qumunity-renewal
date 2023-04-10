76a4e1a1ff1aed520d70dde44bc17d0ad7184e38
mini <0miiii@naver.com>
2023-04-10 19:24:32 +0900

    Implement the logic to write a post on request

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,3 +1,42 @@
 import { Request, Response } from "express";
+import UserModel from "../models/userModel";
+import PostModel from "../models/postModel";
 
-export const createPost = (req: Request, res: Response) => {};
+interface ICreateRquest extends Request {
+  decodedToken: { email: string };
+  title: string;
+  content: string;
+  tags: string[];
+}
+
+export const createPost = async (
+  req: Request<{}, {}, ICreateRquest>,
+  res: Response
+) => {
+  try {
+    const postinfo = new PostModel({
+      title: req.body.title,
+      content: req.body.content,
+      tags: req.body.tags,
+    });
+    const userinfo = await UserModel.findOneAndUpdate(
+      {
+        email: req.body.decodedToken.email,
+      },
+      { $inc: { questions: 1 } }
+    );
+
+    postinfo.author = userinfo._id;
+    await postinfo.save();
+    return res.status(200).json({
+      success: true,
+      message: "글 작성에 성공하였습니다",
+      data: postinfo,
+    });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(400)
+      .json({ success: false, message: "글 작성에 실패하였습니다" });
+  }
+};
