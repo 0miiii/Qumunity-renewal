578495922422bf8d08a0a3ae2c722761a3afb8a4
mini <0miiii@naver.com>
2023-04-11 21:27:00 +0900

    Implements function that requests data from server and renders it

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
 import { Link } from "react-router-dom";
 import Post from "../../components/Post/Post";
 import * as Styled from "./MainPage.style";
+import instance from "../../apis/intance";
+
+interface IRes {
+  success: boolean;
+  message: string;
+  data: IPost[];
+}
+interface IPost {
+  _id: string;
+  title: string;
+  content: string;
+  tags: string[];
+  answers: number;
+  views: number;
+  votes: number;
+  author: IUser;
+  createAt: string;
+  updateAt: string;
+}
+
+interface IUser {
+  _id: string;
+  email: string;
+  nickname: string;
+  photoURL: string;
+  questions: number;
+}
 
 const MainPage = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const getPostRequest = async () => {
+    setLoading(true);
+
+    try {
+      const response = await instance.get<IRes>("/post/getposts");
+      setPosts(response.data.data);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        setError(err);
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPostRequest();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <Styled.Container>
       <Styled.Top>
@@ -14,8 +75,9 @@ const MainPage = () => {
         <input type="text" />
         <div>정렬버튼</div>
       </Styled.FilterGroup>
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
     </Styled.Container>
   );
 };
