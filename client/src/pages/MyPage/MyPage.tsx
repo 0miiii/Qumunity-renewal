4edc1bd3c46c58772a518e9a94ad15fd5bf925f1
mini <0miiii@naver.com>
2023-04-12 16:43:35 +0900

    Add profile component after removing temporary profile

--- client/src/pages/MyPage/MyPage.tsx
+++ client/src/pages/MyPage/MyPage.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import MyPageTabs from "../../components/MyPageTabs/MyPageTabs";
+import Profile from "../../components/Profile/Profile";
 import * as Styled from "./MyPage.style";
 
 const MyPage = () => {
   return (
     <Styled.Container>
-      <Styled.Profile>
-        <img src="" alt="" />
-        <div>
-          <span>닉네임</span>
-          <span>Qestions 2</span>
-          <span>Answers 3</span>
-        </div>
-      </Styled.Profile>
+      <Profile />
       <MyPageTabs />
     </Styled.Container>
   );
