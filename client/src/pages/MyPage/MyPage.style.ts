dbf744fbf30ed4899c4d0f4893b6064218cffc3d
mini <0miiii@naver.com>
2023-04-12 15:52:40 +0900

    Add mypage frame

--- client/src/pages/MyPage/MyPage.style.ts
+++ client/src/pages/MyPage/MyPage.style.ts
@@ -3,6 +3,28 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 840px;
   height: 600px;
-  border: 1px solid red;
   margin: 30px auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Profile = styled.div`
+  border: 1px solid red;
+  display: flex;
+  gap: 10px;
+
+  > img {
+    border: 1px solid green;
+    border-radius: 5px;
+    width: 70px;
+    height: 70px;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
 `;
--- client/src/pages/MyPage/MyPage.tsx
+++ client/src/pages/MyPage/MyPage.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import MyPageTabs from "../../components/MyPageTabs/MyPageTabs";
 import * as Styled from "./MyPage.style";
 
 const MyPage = () => {
-  return <Styled.Container>MyPage</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.Profile>
+        <img src="" alt="" />
+        <div>
+          <span>닉네임</span>
+          <span>Qestions 2</span>
+          <span>Answers 3</span>
+        </div>
+      </Styled.Profile>
+      <MyPageTabs />
+    </Styled.Container>
+  );
 };
 
 export default MyPage;
