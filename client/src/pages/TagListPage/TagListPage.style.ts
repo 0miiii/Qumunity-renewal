e55e181517cd424dfc1bbdd13cd9a455d93b9d03
mini <0miiii@naver.com>
2023-04-12 14:29:17 +0900

    Add tag list page frame

--- client/src/pages/TagListPage/TagListPage.style.ts
+++ client/src/pages/TagListPage/TagListPage.style.ts
@@ -3,6 +3,33 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 840px;
   height: 600px;
-  border: 1px solid red;
   margin: 30px auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const FilterGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const TagList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+
+  > li {
+    border: 1px solid red;
+    padding: 10px;
+    height: 200px;
+  }
+`;
+
+export const Tag = styled.div`
+  display: inline-block;
+  background-color: aquamarine;
+  border-radius: 5px;
+  padding: 5px;
 `;
--- client/src/pages/TagListPage/TagListPage.tsx
+++ client/src/pages/TagListPage/TagListPage.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import * as Styled from "./TagListPage.style";
 
 const TagListPage = () => {
-  return <Styled.Container>TagListPage</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.FilterGroup>
+        <input type="text" />
+      </Styled.FilterGroup>
+      <Styled.TagList>
+        {new Array(9).fill(0).map((_, idx) => (
+          <li key={idx}>
+            <Styled.Tag>#javascript</Styled.Tag>
+          </li>
+        ))}
+      </Styled.TagList>
+    </Styled.Container>
+  );
 };
 
 export default TagListPage;
