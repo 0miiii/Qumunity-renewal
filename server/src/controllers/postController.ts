6dfb23cc11aa4eb95c25dab42c9e478f259d0113
0miiii <0miiii@naver.com>
2023-04-12 21:05:04 +0900

    Add get post controller

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -2,15 +2,19 @@ import { Request, Response } from "express";
 import * as UserService from "../services/userService";
 import * as PostService from "../services/postService";
 
-interface ICreateRquest extends Request {
+interface ISignUpRequest extends Request {
   decodedToken: { _id: string };
   title: string;
   content: string;
   tags: string[];
 }
 
+interface IGetPost {
+  postId: string;
+}
+
 export const signUp = async (
-  req: Request<{}, {}, ICreateRquest>,
+  req: Request<{}, {}, ISignUpRequest>,
   res: Response
 ) => {
   try {
@@ -43,6 +47,26 @@ export const getAllPost = async (_: Request, res: Response) => {
       success: true,
       message: "모든 게시물을 응답합니다",
       data: posts,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      success: false,
+      message: "모든 게시물을 불러오는데 실패하였습니다",
+    });
+  }
+};
+
+export const getPost = async (
+  req: Request<{}, {}, IGetPost>,
+  res: Response
+) => {
+  try {
+    const post = await PostService.findPost(req.body.postId);
+    return res.status(200).json({
+      success: true,
+      message: "게시물을 응답합니다",
+      data: post,
     });
   } catch (err) {
     console.log(err);
