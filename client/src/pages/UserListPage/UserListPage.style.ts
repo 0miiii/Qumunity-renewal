2b4c5dc31bbc60c590d71b8fafbfeff23896fb8f
mini <0miiii@naver.com>
2023-04-12 14:02:44 +0900

    Add user list page frame

--- client/src/pages/UserListPage/UserListPage.style.ts
+++ client/src/pages/UserListPage/UserListPage.style.ts
@@ -3,6 +3,39 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 840px;
   height: 600px;
-  border: 1px solid red;
   margin: 30px auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const UserList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`;
+
+export const FilterGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Profile = styled.li`
+  border: 1px solid red;
+  display: flex;
+  gap: 10px;
+
+  > img {
+    border: 1px solid green;
+    border-radius: 5px;
+    width: 70px;
+    height: 70px;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
 `;
--- client/src/pages/UserListPage/UserListPage.tsx
+++ client/src/pages/UserListPage/UserListPage.tsx
@@ -2,7 +2,29 @@ import React from "react";
 import * as Styled from "./UserListPage.style";
 
 const UserListPage = () => {
-  return <Styled.Container>UserListPage</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.FilterGroup>
+        <input type="text" />
+        <div>
+          <button>qestion</button>
+          <button>answer</button>
+        </div>
+      </Styled.FilterGroup>
+      <Styled.UserList>
+        {new Array(9).fill(0).map((_, idx) => (
+          <Styled.Profile key={idx}>
+            <img src="" alt="" />
+            <div>
+              <span>닉네임</span>
+              <span>Qestions 2</span>
+              <span>Answers 3</span>
+            </div>
+          </Styled.Profile>
+        ))}
+      </Styled.UserList>
+    </Styled.Container>
+  );
 };
 
 export default UserListPage;
