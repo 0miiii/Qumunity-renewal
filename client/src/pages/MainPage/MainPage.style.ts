70ad6bb12ae9e7942315865480490915ecff3841
0miiii <0miiii@naver.com>
2023-04-13 00:28:13 +0900

    Design

--- client/src/pages/MainPage/MainPage.style.ts
+++ client/src/pages/MainPage/MainPage.style.ts
@@ -2,10 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   max-width: 840px;
-  height: 600px;
-  border: 1px solid red;
   margin: 30px auto;
-
   display: flex;
   flex-direction: column;
   gap: 10px;
