432a161351876a0408255490f41d54e5932e0f8e
mini <0miiii@naver.com>
2023-04-12 16:23:24 +0900

    Add Tag component

---
+++ client/src/components/Tag/Tag.style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.span`
+  display: inline-block;
+  background-color: aquamarine;
+  border-radius: 5px;
+  padding: 5px;
+`;
---
+++ client/src/components/Tag/Tag.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as Styled from "./Tag.style";
+
+interface Props {
+  name: string;
+}
+
+const Tag: React.FC<Props> = ({ name }) => {
+  return <Styled.Container>#{name}</Styled.Container>;
+};
+
+export default Tag;
