984afa92bc26e4fe9bead54b4e7df9669b064b09
0miiii <0miiii@naver.com>
2023-04-13 16:52:59 +0900

    Use getMyInfo controller

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { validateToken } from "../middlewares/authMiddleware";
 import * as userController from "../controllers/userController";
 
 const router = Router();
 
 router.get("/auth", userController.isTokenValid);
 router.get("/getUsers", userController.getAllUserInfo);
+router.get("/getMyInfo", validateToken, userController.getMyInfo);
 router.post("/signUp", userController.signUp);
 router.post("/signIn", userController.signIn);
 
