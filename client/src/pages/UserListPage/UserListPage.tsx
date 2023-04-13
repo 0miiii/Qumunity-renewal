eb46fb968e3e0f9860bf6910d54b4a5b95ea0806
0miiii <0miiii@naver.com>
2023-04-13 15:05:01 +0900

    Add getAllUserInfoRequest

--- client/src/pages/UserListPage/UserListPage.tsx
+++ client/src/pages/UserListPage/UserListPage.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import instance from "../../apis/intance";
 import Profile from "../../components/Profile/Profile";
+import { IUser } from "../../types/user";
 import * as Styled from "./UserListPage.style";
+import axios, { AxiosError } from "axios";
+
+interface IResponse {
+  message: string;
+  data: IUser[];
+}
 
 const UserListPage = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const getAllUserInfoRequest = async () => {
+    setLoading(true);
+    try {
+      const response = await instance.get<IResponse>("/user/getUsers");
+      setUsers(response.data.data);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        setError(err);
+      } else {
+        console.log(err);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllUserInfoRequest();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <Styled.Container>
       <Styled.FilterGroup>
@@ -13,9 +53,9 @@ const UserListPage = () => {
         </div>
       </Styled.FilterGroup>
       <Styled.UserList>
-        {new Array(9).fill(0).map((_, idx) => (
-          <li key={idx}>
-            <Profile />
+        {users.map((user) => (
+          <li key={user._id}>
+            <Profile user={user} />
           </li>
         ))}
       </Styled.UserList>
