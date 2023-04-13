82cbbac33e3f80ef0fe4b20c6530a716ff182089
0miiii <0miiii@naver.com>
2023-04-13 15:03:25 +0900

    Use getAllUserInfo to router

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -4,6 +4,7 @@ import * as userController from "../controllers/userController";
 const router = Router();
 
 router.get("/auth", userController.isTokenValid);
+router.get("/getUsers", userController.getAllUserInfo);
 router.post("/signUp", userController.signUp);
 router.post("/signIn", userController.signIn);
 
