3cb055cee5c43293efcfa8f4bfc83c3b7cefa14f
0miiii <0miiii@naver.com>
2023-04-13 15:04:30 +0900

    Design

--- client/src/components/Profile/Profile.style.ts
+++ client/src/components/Profile/Profile.style.ts
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  border: 1px solid red;
   display: flex;
   gap: 10px;
 
   > img {
-    border: 1px solid green;
     border-radius: 5px;
     width: 70px;
     height: 70px;
