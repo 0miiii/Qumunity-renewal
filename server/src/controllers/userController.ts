951c6b35a799a50faafcf7552aae0c4e8e56dc29
mini <0miiii@naver.com>
2023-04-16 20:49:28 +0900

    Fix sending response twice

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -14,12 +14,12 @@ export const signUp = async (req: Request, res: Response) => {
       photoURL: `https://source.boringavatars.com/beam/130/${req.body.nickname}?square`,
     });
 
-    res.status(200).send({
+    return res.status(200).send({
       message: "Qumunity에 오신 것을 환영합니다",
       token: createToken({ _id: user._id }),
     });
   } catch (err) {
-    res.status(400).send({
+    return res.status(400).send({
       message: "회원가입에 실패했습니다",
     });
   }
@@ -31,18 +31,18 @@ export const signIn = async (req: Request, res: Response) => {
     const isValid = await verifyPassword(req.body.password, user.password);
 
     if (!isValid) {
-      res.status(400).send({
+      return res.status(400).send({
         message: "비밀번호가 일치하지 않습니다",
       });
     }
 
-    res.status(200).send({
+    return res.status(200).send({
       message: "Qumunity에 오신 것을 환영합니다",
       token: createToken({ _id: user._id }),
     });
   } catch (err) {
-    res.status(400).send({
-      message: "로그인에 실패했습니다",
+    return res.status(400).send({
+      message: `로그인에 실패했습니다 ${err}`,
     });
   }
 };
