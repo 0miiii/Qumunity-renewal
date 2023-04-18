99d140adf990aa84b21e54f0ae95958fcf4c924d
0miiii <0miiii@naver.com>
2023-04-18 23:50:00 +0900

    Refactor log to error

--- server/src/libs/mongoose.ts
+++ server/src/libs/mongoose.ts
@@ -7,5 +7,5 @@ export const dbConnect = async () => {
   await mongoose
     .connect(process.env.MONGOOSE_API_KEY)
     .then(() => console.log(`mongoose connection successful`))
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
 };
