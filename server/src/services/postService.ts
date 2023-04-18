1cf9b624d24d0361eec12d5b2e303c6b689ad214
mini <0miiii@naver.com>
2023-04-18 20:04:25 +0900

    Add update, delete service

--- server/src/services/postService.ts
+++ server/src/services/postService.ts
@@ -6,6 +6,11 @@ interface IRequestPost {
   tags: string[];
   author: string;
 }
+interface IUpdateRequestPost {
+  title: string;
+  content: string;
+  tags: string[];
+}
 
 export const createPost = async (post: IRequestPost): Promise<IPost> => {
   const newPost = await new PostModel(post).save();
@@ -32,5 +37,23 @@ export const findPost = async (postId: string): Promise<IPost> => {
     return await PostModel.findOne({ _id: postId }).populate("author");
   } catch (err) {
     throw new Error(`게시물을 찾는 중에 에러가 발생했습니다 ${err}`);
+  }
+};
+
+export const updatePost = async (postId: string, post: IUpdateRequestPost) => {
+  try {
+    return await PostModel.findByIdAndUpdate(postId, {
+      $set: post,
+    });
+  } catch (err) {
+    throw new Error(`게시물 수정 중 에러 ${err}`);
+  }
+};
+
+export const deletePost = async (postId: string) => {
+  try {
+    return await PostModel.findByIdAndDelete(postId);
+  } catch (err) {
+    throw new Error(`게시물 삭제 중 에러 ${err}`);
   }
 };
