fa5c54ae113a840bb87480f1bc876fdc5e1dafeb
0miiii <0miiii@naver.com>
2023-04-18 21:11:42 +0900

    Add 'api' to explicitly express that it is an api

--- client/src/apis/intance.ts
+++ client/src/apis/intance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessTokenFromLocalStorage } from "../utils/tokenHandler";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000/api",
 });
 
 instance.interceptors.request.use(
