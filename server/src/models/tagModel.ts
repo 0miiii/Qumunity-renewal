0b1838be66710ef5019a5bbf51a4be2eff0c497b
mini <0miiii@naver.com>
2023-04-19 14:25:43 +0900

    Add description

--- server/src/models/tagModel.ts
+++ server/src/models/tagModel.ts
@@ -2,12 +2,14 @@ import { Schema, model, Document } from "mongoose";
 
 export interface ITag extends Document {
   name: string;
+  description: string;
   count: number;
 }
 
 const tagSchema = new Schema<ITag>(
   {
     name: String,
+    description: { type: String, default: "태그를 설명해주세요" },
     count: { type: Number, default: 0 },
   },
   { collection: "tags" }
