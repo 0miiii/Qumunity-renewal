61bdf2cb8bee52bccf7568ca75fdb4c1d66722eb
mini <0miiii@naver.com>
2023-04-19 17:03:38 +0900

    Add react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { store } from "./store/store";
 import App from "./App";
 import GlobalStyle from "./styles/Global.style";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
-    <GlobalStyle />
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+      <ReactQueryDevtools />
+    </Provider>
+  </QueryClientProvider>
 );
