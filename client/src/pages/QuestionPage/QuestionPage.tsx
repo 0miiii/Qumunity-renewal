084569a23ac0ed4c595bd20c96b425e6f75b7a20
mini <0miiii@naver.com>
2023-04-20 01:00:02 +0900

    Refactor to use mutation to manage server data

--- client/src/pages/QuestionPage/QuestionPage.tsx
+++ client/src/pages/QuestionPage/QuestionPage.tsx
@@ -1,27 +1,43 @@
 import React, { useRef, useState } from "react";
+import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import TagInput from "../../components/TagInput/TagInput";
+import { createPost } from "../../apis/post";
 import * as Styled from "./QuestionPage.style";
-import instance from "../../apis/intance";
+
+interface IEnteredPost {
+  title: string;
+  content: string;
+  tags: string[];
+}
 
 const QuestionPage = () => {
   const routeTo = useNavigate();
   const [tags, setTags] = useState<string[]>([]);
   const titleInputRef = useRef<HTMLInputElement>(null);
   const editorRef = useRef<Editor>(null);
+  const { mutate, isLoading, isSuccess } = useMutation((post: IEnteredPost) =>
+    createPost(post)
+  );
 
   const submitHandler = async () => {
-    await instance.post("/post/register", {
-      title: titleInputRef.current?.value,
-      content: editorRef.current?.getInstance().getHTML(),
+    mutate({
+      title: titleInputRef.current?.value as string,
+      content: editorRef.current?.getInstance().getHTML() as string,
       tags,
     });
-
-    routeTo("/");
   };
+
+  if (isLoading) {
+    return <div>로딩 중</div>;
+  }
+
+  if (isSuccess) {
+    return routeTo("/");
+  }
 
   return (
     <Styled.Container onSubmit={submitHandler}>
