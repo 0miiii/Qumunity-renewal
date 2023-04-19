6f7de5149f313c21e47b6cc709cbb8f345a1c4a0
mini <0miiii@naver.com>
2023-04-20 03:23:24 +0900

    Add create, getMyPost api

--- client/src/apis/post.ts
+++ client/src/apis/post.ts
@@ -8,3 +8,19 @@ export const getPosts = () => {
 export const getPost = (postId: string) => {
   return instance.get<IPost>(`/post/${postId}`).then((res) => res.data);
 };
+
+export const getMyPost = (page: number, limit: number) => {
+  return instance
+    .get<IPost[]>(`/post/mypost?page=${page}&limit=${limit}`)
+    .then((res) => res.data);
+};
+
+interface IEnteredPost {
+  title: string;
+  content: string;
+  tags: string[];
+}
+
+export const createPost = (post: IEnteredPost) => {
+  return instance.post<IPost>("/post/register", post).then((res) => res.data);
+};
