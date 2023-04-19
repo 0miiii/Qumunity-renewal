bbebc5d5bb15793b058e617998c2ffccff5a4965
mini <0miiii@naver.com>
2023-04-19 13:57:23 +0900

    Add create find controller

--- server/src/controllers/answerController.ts
+++ server/src/controllers/answerController.ts
@@ -34,3 +34,29 @@ export const createAnswer = async (
     return res.status(400).json("답변 작성에 실패하였습니다");
   }
 };
+
+export const findAnswer = async (
+  req: Request<{}, {}, { id: string }>,
+  res: Response
+) => {
+  try {
+    const answer = await AnswerService.findAnswer(req.body.id);
+    return res.status(200).json(answer);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json("답변을 불러오는데 실패하였습니다");
+  }
+};
+
+export const deleteAnswer = async (
+  req: Request<{}, {}, { answerId: string }>,
+  res: Response
+) => {
+  try {
+    await AnswerService.deleteAnswer(req.body.answerId);
+    return res.status(200).json("답변을 삭제했습니다");
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json("답변을 불러오는데 실패하였습니다");
+  }
+};
