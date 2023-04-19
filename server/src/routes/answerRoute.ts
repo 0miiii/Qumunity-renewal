d74cfa49121e16a02ff82a9da94da090a2a253e9
mini <0miiii@naver.com>
2023-04-19 13:57:40 +0900

    Add create find route

--- server/src/routes/answerRoute.ts
+++ server/src/routes/answerRoute.ts
@@ -4,6 +4,8 @@ import * as answerController from "../controllers/answerController";
 
 const router = Router();
 
+router.post("/", answerController.findAnswer);
 router.post("/register", validateToken, answerController.createAnswer);
+router.post("/delete", validateToken, answerController.deleteAnswer);
 
 export default router;
