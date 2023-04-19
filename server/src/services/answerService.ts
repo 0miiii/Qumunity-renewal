faf51f7c72f234b817a570bafa594f2df0afb9c2
mini <0miiii@naver.com>
2023-04-19 13:57:05 +0900

    Add create find service

--- server/src/services/answerService.ts
+++ server/src/services/answerService.ts
@@ -16,3 +16,21 @@ export const createAnswer = async (answer: ISaveAnswer): Promise<IAnswer> => {
     throw new Error(`답변 생성 에러 ${err}`);
   }
 };
+
+export const findAnswer = async (id: string): Promise<IAnswer[]> => {
+  try {
+    return await AnswerModel.find({ $or: [{ author: id }, { post: id }] });
+  } catch (err) {
+    throw new Error(`답변 찾기 에러 ${err}`);
+  }
+};
+
+export const updateAnswer = async () => {};
+
+export const deleteAnswer = async (answerId: string): Promise<IAnswer> => {
+  try {
+    return await AnswerModel.findByIdAndDelete(answerId);
+  } catch (err) {
+    throw new Error(`답변 삭제 에러 ${err}`);
+  }
+};
